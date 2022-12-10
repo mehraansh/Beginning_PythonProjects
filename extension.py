62fed6c1baeed18e70ca9541a943937899e01dfc
mehraansh <99342128+mehraansh@users.noreply.github.com>
2022-12-10 22:18:09 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,2 +1,6 @@
+import re
 fileName = input("Input the Filename: ")
+# code to find characters in string
+res1 = " ".join(re.split("[.py]*", fileName))
+ # printing resultant string
 print("The extension of the file is Python" )
